14683c463df27e5433646a5c7c89f277a1a521cf
binzhi0710 <2022221511@stu.cqmu.edu.cn>
2025-12-05 00:18:11 +0800

    Update 02HDL.R

--- 01Trait-pair level/02Global Genetic Correlation/02HDL.R
+++ 01Trait-pair level/02Global Genetic Correlation/02HDL.R
@@ -1,38 +1,25 @@
-##### HDL Analysis #####
 library(HDL)
 library(data.table)
 library(doSNOW)
 setDTthreads(32)
 
-# Set paths
 LD.path <- "./LDimpute"
 personality_file <- "./02HDLOA/AllOA.txt.gz"
 pd_path <- "./00metabo/HDLpre"
 output_dir <- "./02HDLresults"
 
-# Get list of pd files
 pd_files <- list.files(pd_path, pattern = "\\.txt.gz$", full.names = TRUE)
 
-# Ensure output directory exists
-if (!dir.exists(output_dir)) {
-  dir.create(output_dir)
-}
+if (!dir.exists(output_dir)) dir.create(output_dir)
 
-# Read the specified personality file
-cat("Reading personality file: ", basename(personality_file), "\n")
 gwas1 <- fread(personality_file)
 
-# Loop through pd_files
 for (d_file in pd_files) {
-  cat("Processing d_file: ", basename(d_file), "\n")
   
-  # Read d_file
   gwas2 <- fread(d_file)
   
-  # HDL analysis
   res.HDL <- HDL.rg.parallel(gwas1, gwas2, LD.path, numCores = 4)
   
-  # Create data frame
   res_df <- data.frame(
     rg = res.HDL$rg,
     rg_se = res.HDL$rg.se,
@@ -48,12 +35,10 @@ for (d_file in pd_files) {
     eigen_use = res.HDL$eigen.use
   )
   
-  # Build output file name
   p_file_name <- basename(sub("\\.txt.gz$", "", personality_file))
   d_file_name <- basename(sub("\\.txt.gz$", "", d_file))
-  output_file_name <- paste(p_file_name, d_file_name, sep = "-", collapse = NULL)
+  output_file_name <- paste(p_file_name, d_file_name, sep = "-")
   output_file_path <- file.path(output_dir, paste(output_file_name, "csv", sep = "."))
   
-  # Save results
   write.csv(res_df, output_file_path, row.names = FALSE)
 }
