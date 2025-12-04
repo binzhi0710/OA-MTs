4e53adb84eb75a9eb5d998205dc900e15f51c36f
binzhi0710 <2022221511@stu.cqmu.edu.cn>
2025-12-05 00:28:12 +0800

    Update 03coloc.R

--- 02GWAS level/03coloc.R
+++ 02GWAS level/03coloc.R
@@ -1,4 +1,3 @@
-# Load required libraries
 library(dplyr)
 library(coloc)
 library(data.table)
@@ -6,103 +5,84 @@ library(readxl)
 
 setDTthreads(32)
 
-# Read MTAGcolocloci.xlsx file
 fuma_data <- read_excel("MTAGcolocloci.xlsx")
 
-# Create an empty data frame to store results
 results <- data.frame(
-  PSY = character(),
-  osteoarthritis = character(),
-  chr = numeric(),
-  start = numeric(),
-  end = numeric(),
-  df1_file = character(),
-  df2_file = character(),
-  PP.H0.abf = numeric(),
-  PP.H1.abf = numeric(),
-  PP.H2.abf = numeric(),
-  PP.H3.abf = numeric(),
-  PP.H4.abf = numeric(),
-  stringsAsFactors = FALSE
+  PSY=character(),
+  osteoarthritis=character(),
+  chr=numeric(),
+  start=numeric(),
+  end=numeric(),
+  df1_file=character(),
+  df2_file=character(),
+  PP.H0.abf=numeric(),
+  PP.H1.abf=numeric(),
+  PP.H2.abf=numeric(),
+  PP.H3.abf=numeric(),
+  PP.H4.abf=numeric(),
+  stringsAsFactors=FALSE
 )
 
-# Iterate through each row to read corresponding files and perform analysis
 for (i in 1:nrow(fuma_data)) {
-  PSY_id <- fuma_data$PSY[i]
-  osteoarthritis_id <- fuma_data$osteoarthritis[i]
+  x1 <- fuma_data$PSY[i]
+  x2 <- fuma_data$osteoarthritis[i]
+  c1 <- as.numeric(fuma_data$chr[i])
+  s1 <- as.numeric(fuma_data$start[i])
+  e1 <- as.numeric(fuma_data$end[i])
   
-  # Get chr, start, and end for the current row
-  chr <- as.numeric(fuma_data$chr[i])
-  start <- as.numeric(fuma_data$start[i])
-  end <- as.numeric(fuma_data$end[i])
+  f1 <- file.path("00PSYGWAS_noMHC", paste0(x1, ".txt"))
+  f2 <- file.path("00OAGWAS_noMHC", paste0(x2, ".txt"))
   
-  # Construct file paths
-  PSY_file <- file.path("00PSYGWAS_noMHC", paste0(PSY_id, ".txt"))
-  osteoarthritis_file <- file.path("00OAGWAS_noMHC", paste0(osteoarthritis_id, ".txt"))
+  d1 <- fread(f1)
+  d2 <- fread(f2)
   
-  # Read files
-  df1 <- fread(PSY_file, header = TRUE)
-  df2 <- fread(osteoarthritis_file, header = TRUE)
+  d1 <- data.frame(SNP=d1$SNP, chrom=d1$chr, pos=d1$pos, A1=d1$A1, A2=d1$A2, beta=d1$beta, se=d1$se, eaf=d1$eaf)
+  d1$MAF <- ifelse(d1$eaf < 0.5, d1$eaf, 1 - d1$eaf)
+  d1 <- subset(d1, chrom == c1 & pos >= s1 & pos <= e1)
   
-  # Preprocess df1
-  df1 <- data.frame(SNP = df1$SNP, chrom = df1$chr, pos = df1$pos, A1 = df1$A1, A2 = df1$A2, beta = df1$beta, se = df1$se, eaf = df1$eaf)
-  df1$MAF <- ifelse(df1$eaf < 0.5, df1$eaf, 1 - df1$eaf)
+  d2 <- data.frame(SNP=d2$SNP, A1=d2$A1, A2=d2$A2, beta=d2$beta, se=d2$se)
   
-  # Filter region for colocalization
-  df1 <- subset(df1, chrom == chr & pos >= start & pos <= end)
+  m <- merge(d1, d2, by="SNP")
+  m <- m[!duplicated(m$SNP),]
+  m$A1.y <- toupper(m$A1.y)
+  m$A2.y <- toupper(m$A2.y)
   
-  # Preprocess df2
-  df2 <- data.frame(SNP = df2$SNP, A1 = df2$A1, A2 = df2$A2, beta = df2$beta, se = df2$se)
+  m <- m %>%
+    filter((A1.x==A1.y & A2.x==A2.y) | (A1.x==A2.y & A2.x==A1.y)) %>%
+    mutate(beta.y = ifelse(A1.x==A1.y, beta.y, -beta.y))
   
-  # Merge data frames
-  dfall <- merge(df1, df2, by = "SNP")
-  dfall <- dfall[!duplicated(dfall$SNP), ]
+  m$VAR1 <- m$se.x^2
+  m$VAR2 <- m$se.y^2
+  m <- m[m$VAR1 != 0 & m$VAR2 != 0,]
+  m <- na.omit(m)
   
-  # Convert A1.y and A2.y to uppercase
-  dfall$A1.y <- toupper(dfall$A1.y)
-  dfall$A2.y <- toupper(dfall$A2.y)
+  c1d <- data.frame(beta=m$beta.x, varbeta=m$VAR1, snp=m$SNP, MAF=m$MAF)
+  c2d <- data.frame(beta=m$beta.y, varbeta=m$VAR2, snp=m$SNP)
+  c1d <- as.list(c1d)
+  c2d <- as.list(c2d)
   
-  # Filter and adjust beta.y
-  dfall <- dfall %>%
-    filter((A1.x == A1.y & A2.x == A2.y) | (A1.x == A2.y & A2.x == A1.y)) %>%
-    mutate(beta.y = ifelse(A1.x == A1.y, beta.y, -beta.y))
+  c1d$type <- "cc"
+  c2d$type <- "cc"
   
-  dfall$VAR1 <- dfall$se.x^2
-  dfall$VAR2 <- dfall$se.y^2
-  dfall <- dfall[dfall$VAR1 != 0 & dfall$VAR2 != 0, ]
+  r <- coloc.abf(c1d, c2d, p1=1e-4, p2=1e-4, p12=1e-5)
   
-  # Remove rows with NA values
-  dfall <- na.omit(dfall)
-  
-  cdf1 <- data.frame(beta = dfall$beta.x, varbeta = dfall$VAR1, snp = dfall$SNP, MAF = dfall$MAF)
-  cdf2 <- data.frame(beta = dfall$beta.y, varbeta = dfall$VAR2, snp = dfall$SNP)
-  
-  cdf1 <- as.list(cdf1)
-  cdf2 <- as.list(cdf2)
-  
-  cdf1$type <- "cc"  # Continuous variable
-  cdf2$type <- "cc"  # Binary variable
-  
-  colocresult <- coloc.abf(cdf1, cdf2, p1 = 1e-4, p2 = 1e-4, p12 = 1e-5)
-  
-  # Extract colocalization results and store
-  result_row <- data.frame(
-    PSY = PSY_id,
-    osteoarthritis = osteoarthritis_id,
-    chr = chr,
-    start = start,
-    end = end,
-    df1_file = PSY_file,
-    df2_file = osteoarthritis_file,
-    PP.H0.abf = colocresult$summary[["PP.H0.abf"]],
-    PP.H1.abf = colocresult$summary[["PP.H1.abf"]],
-    PP.H2.abf = colocresult$summary[["PP.H2.abf"]],
-    PP.H3.abf = colocresult$summary[["PP.H3.abf"]],
-    PP.H4.abf = colocresult$summary[["PP.H4.abf"]]
+  results <- rbind(
+    results,
+    data.frame(
+      PSY=x1,
+      osteoarthritis=x2,
+      chr=c1,
+      start=s1,
+      end=e1,
+      df1_file=f1,
+      df2_file=f2,
+      PP.H0.abf=r$summary[["PP.H0.abf"]],
+      PP.H1.abf=r$summary[["PP.H1.abf"]],
+      PP.H2.abf=r$summary[["PP.H2.abf"]],
+      PP.H3.abf=r$summary[["PP.H3.abf"]],
+      PP.H4.abf=r$summary[["PP.H4.abf"]]
+    )
   )
-  
-  results <- rbind(results, result_row)
 }
 
-# Save results data frame to CSV file
-write.csv(results, "MTAGlocicoloc_results.csv", row.names = FALSE)
+write.csv(results, "MTAGlocicoloc_results.csv", row.names=FALSE)
